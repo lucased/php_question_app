75cac5336d417f12684c6275ec99dc16bc887176
Lucas Dobbins <lucasdobbins@gmail.com>
2013-05-28 11:42:38 +1000

    Updated question apge and styles

--- qa-create.php
+++ qa-create.php
@@ -17,25 +17,33 @@
 		<?php include "include/header.php" ?>
 
 		<section id="qanda">
+
 			<h2>Q &amp; A Questions</h2>
 			<h3>Create Question</h3>
-			<form>
-				<label for="question">Question</label>
-				<input type="text" name="question" id="questions" class="block" placeholder="Enter Q&amp;A Question">
+			<div id="left">
+				<form>
+					<label for="question">Question</label>
+					<textarea name="question" id="question" class="block" placeholder="Enter Q&amp;A Question, Max character 88" cols="50" rows="3" maxlength="88"></textarea>
 
-				<label for="answer">Answer</label>
-				<input type="text" name="answer" class="block" id="answer" placeholder="Enter Answer">
+					<label for="answer">Answer</label>
+					<input type="text" name="answer" class="block" id="answer" placeholder="Enter Answer" size="50">
 
-				<label for="extra-info">Extra Information</label>
-				<input type="text" name="extra-info" class="block" id="extra-info" placeholder="Enter extra infomtaion about question">
+					<label for="extra-info">Extra Information</label>
+					<input type="text" name="extra-info" class="block" id="extra-info" placeholder="Enter extra infomtaion about question">
 
-				<label for="ref1">Reference 1</label>
-				<input type="text" name="refernece" class="block" id="ref1" placeholder="Enter Reference">
+					<label for="ref1">Reference 1</label>
+					<input type="text" name="refernece" class="block" id="ref1" placeholder="Enter Reference">
 
-				<label for="ref2">Reference 2</label>
-				<input type="text" name="refernece" class="block" id="ref2" placeholder="Enter Reference">
-				
-			</form>
+					<label for="ref2">Reference 2</label>
+					<input type="text" name="refernece" class="block" id="ref2" placeholder="Enter Reference">
+
+				</form>
+			</div>
+
+			<div id="right">
+				<label for="q-id">Question ID</label>
+				<input type="text" name="q-id" class="block" id="q-id" placeholder="Question ID">
+			</div>
 		</section>
 
 		<?php include "include/footer.php" ?>
